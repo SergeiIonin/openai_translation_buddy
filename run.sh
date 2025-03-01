7e3dc42c50fa5b7d8a2f503ab1558cea6e60fb1d
SergeiIonin <sai1042@yandex.ru>
2025-03-01 01:07:07 -0300

    first commit

---
+++ run.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+source config.sh
+
+echo $BASE_PROMPT
+
+if [[ $POINTER -eq -1 ]]; then
+    lines=$(echo $(wc -l $FILE) | awk '{print $1}')
+    pointer=$lines
+else
+    pointer=$POINTER    
+fi
+
+echo $pointer
+
+words=""
+
+wordsExtractor() {
+    count=$WORDS_PER_MSG
+    while [[ $count -gt 0 && $pointer -gt 0 ]]; do
+        echo $pointer
+        w=$(sed -n "${pointer}p" $FILE | awk -F, '{print $3}')
+        if [ count == $WORDS_PER_MSG ]; then
+            words="$w,"
+        else 
+            words="$words $w,"
+        fi
+        ((count--))
+        ((pointer--))
+    done
+    sed -i '' "/^export POINTER=/s/.*/export POINTER=$pointer/" config.sh
+}
+
+wordsExtractor
+
+echo $words
+echo $pointer
+
+resp=""
+
+call_openai() {
+    resp=$(curl "https://api.openai.com/v1/chat/completions" \
+        -H "Content-Type: application/json" \
+        -H "Authorization: Bearer $OPENAI_KEY" \
+        -H "OpenAI-Project: $PROJECT_ID" \
+        --data "{
+            \"model\": \"$MODEL\",
+            \"messages\": [
+                {
+                    \"role\": \"system\",
+                    \"content\": \"You are a helpful assistant.\"
+                },
+                {
+                    \"role\": \"user\",
+                    \"content\": \"$BASE_PROMPT: $words\"
+                }
+            ]
+        }"
+    )
+}
+
+call_openai
+
+content=$(jq '.choices.[-1].message.content' <<< $resp)
+
+body=$(echo $content | sed 's/\\n/\n/g')
+
+echo $body
+
+send_email() {
+    echo "sending email"
+    subject="Regular spanish examples"
+
+    curl --url "smtps://smtp.gmail.com:465" \
+        --ssl-reqd \
+        --mail-from $FROM \
+        --mail-rcpt $TO \
+        --user $FROM:"$APP_PASS" \
+        --upload-file <(echo -e "From: $FROM\nTo: $TO\nSubject: $subject\n\n$body")
+    }
+
+send_email
