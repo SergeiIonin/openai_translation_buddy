64b9ec92c3780e58e3eaf9208076db004986dc2c
SergeiIonin <sai1042@yandex.ru>
2025-03-02 12:21:15 -0300

    add some comments to config

--- config.sh
+++ config.sh
@@ -11,6 +11,6 @@ export MODEL=$MODEL #e.g. "gpt-4o"
 # translations
 export BASE_PROMPT="Don't add introductory words like 'certainly!' etc, just write a number of examples with the following spanish words or phrases with translation to russian, be succinct: "
 export FILE=$CSV_FILE
-export WORDS_PER_MSG=$WORDS_PER_MSG
+export WORDS_PER_MSG=$WORDS_PER_MSG # number of words for exercise in one email message
 export POINTER=-1 # initial value should be -1. Set up before the start and never change unless you start again
 
